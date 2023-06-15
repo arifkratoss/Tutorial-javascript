0a35cb83ed31017598f1d344caaf4daf4f7e164f
arifkratoss <arifkratos21@gmail.com>
2023-06-15 08:13:12 +0700

    menabhakan repo yan baru

---
+++ main-7.js
@@ -0,0 +1,24 @@
+// const close=document.querySelector('.close')
+// const card =document.querySelector('.card')
+
+// close.addEventListener ('click',function (){
+//     card.style.display='none';
+// })
+
+// DOM traversal
+// const close=document.querySelectorAll('.close');
+
+// for(let i= 0; i <close.length; i++){
+//     close[i].addEventListener('click',function (e){
+//         // close[i].parentElement.style.display ='none';
+//         e.target.parentElement.style.display='none';
+//     })
+// }
+
+
+// const close=document.querySelectorAll('.close');
+// close.forEach(function(el) {
+//     el.addEventListener('click', function(e) {
+//         e.target.parentElement.style.display = 'none';
+//     });
+// });
